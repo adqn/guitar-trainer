4407e5bbe2dc43d14c4eb87d0291915c22e8eced
adqn <57331120+adqn@users.noreply.github.com>
2022-04-25 12:49:16 -0700

    misc

---
+++ Pages/Chords.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { 
+  Text,
+  Button,
+  Image,
+  View
+} from 'react-native';
+
+export const Chords = () => {
+
+  return (
+    <View>
+
+    </View>
+  )
+}
